a6e5af2eaee5548ac69d29a03c9351b46ba5f066
muskan612 <muskansahoo2001@gmail.com>
2022-07-10 18:13:09 +0530

    Initial skeleton

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import Login from './components/Login'
+import Signup from './components/Signup';
+import Home from './components/Home';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path='/' element={<Login/>} />
+          <Route path='/signup' element={<Signup/>} />
+          <Route path='/home' element={<Home/>} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
