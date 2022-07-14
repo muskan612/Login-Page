29b6e1434a3cb25ea23672b2f79551975d6fecd7
Devkumar vangar <devkumarvangar@gmail.com>
2022-07-14 14:26:46 +0530

    created login signup page

---
+++ Login-Page-praneethlogin/src/App.js
@@ -0,0 +1,20 @@
+import Login from './components/Login'
+import Signup from './components/Signup';
+import Home from './components/Home';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path='/Login' element={<Login/>} />
+          <Route path='/signup' element={<Signup/>} />
+          <Route path='/home' element={<Home/>} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
