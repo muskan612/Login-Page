712f12eb8ec8ca8ebdb35c2835b28552cbd0409a
muskan612 <muskansahoo2001@gmail.com>
2022-07-15 13:06:31 +0530

    rough design for homepage

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,32 @@
-import React from 'react'
+import React from 'react';
+import './Home.css';
 
 const Home = () => {
-  return (
-    <div>Home</div>
-  )
-}
+	return (
+		<div className='container'>
+			<div className='parallax'>
+				<section>Scroll Down</section>
+			</div>
+			<div className='parallax' style={{ height: '150vh' }}>
+				<div className='block'>
+					<p>
+						Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum vel
+						voluptatibus architecto? Quibusdam quae eveniet laudantium dolores,
+						nam aliquam cum doloremque consequatur iste quis repellendus, fugiat
+						placeat optio! Quisquam praesentium eius aliquid molestiae
+						perspiciatis, explicabo illo qui. Pariatur similique saepe, fuga ut
+						dolor sed soluta cumque sapiente, cupiditate explicabo praesentium.
+						Lorem, ipsum dolor sit amet consectetur adipisicing elit. Provident
+						sapiente suscipit quod! Reprehenderit maxime labore omnis corporis
+						error quibusdam exercitationem quam ipsum, quos in magni porro
+						voluptas? Enim voluptatibus dolorum nam, sequi impedit rem qui amet
+						neque quidem numquam accusamus labore totam. Rerum voluptates
+						delectus inventore ratione excepturi repellendus voluptatum.
+					</p>
+				</div>
+			</div>
+		</div>
+	);
+};
 
-export default Home
+export default Home;
