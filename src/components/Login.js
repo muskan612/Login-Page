44ffa92d5f5ebe4ecf62e98dcbc27f3ebca517c0
muskan612 <muskansahoo2001@gmail.com>
2022-07-16 13:08:05 +0530

    done

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,70 +1,96 @@
-import React, { useState } from 'react'
-import './Login.css'
-import {Link} from 'react-router-dom'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import {  faGoogle,faFacebook,faLinkedin } from '@fortawesome/free-brands-svg-icons' // <-- import styles to be used
-import {  faEye,faEyeSlash } from '@fortawesome/free-solid-svg-icons'
-
-
+import React, { useState } from 'react';
+import './Login.css';
+import '../index.css';
+import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+	faGoogle,
+	faFacebook,
+	faLinkedin,
+} from '@fortawesome/free-brands-svg-icons'; // <-- import styles to be used
+import { faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons';
 
 const Login = () => {
-  const myStyle={
-    
-    height:"25px",
-    width:"25px"
-    
-  }
-  const [mode,setMode]=useState(faEye)
-  const [visibility,setVisibility]=useState("password")
-  const toggleButton=()=>{
-    if(mode===faEye)
-    {setMode(faEyeSlash);
-    setVisibility("text");
-    }
-    else {setMode(faEye);
-    setVisibility("password");}
+	const myStyle = {
+		height: '25px',
+		width: '25px',
+	};
+	const [mode, setMode] = useState(faEye);
+	const [visibility, setVisibility] = useState('password');
+	const toggleButton = () => {
+		if (mode === faEye) {
+			setMode(faEyeSlash);
+			setVisibility('text');
+		} else {
+			setMode(faEye);
+			setVisibility('password');
+		}
+	};
 
-  }
- 
-  return (
-  <div style={{fontFamily: "'Open Sans', sans-serif"}}> 
+	return (
+		<div style={{ fontFamily: "'Open Sans', sans-serif" }}>
+			<div className='card'>
+				<h1>Log in</h1>
+				<form id='form1' action='./Home'>
+					<div className='input1'>
+						<lable forHtml='email' className='email '>
+							Email address
+						</lable>
 
- 
+						<input
+							type='email'
+							required
+							className='form-control '
+							id='exampleFormControlInput1'
+							placeholder='name@gmail.com'
+						/>
+					</div>
+					<div className='input2'>
+						<lable forHtml='password' className='password'>
+							Password
+						</lable>
+						<div className='buttonIn'>
+							<input
+								id='eye'
+								type={visibility}
+								required
+								pattern='(?=.*[a-z]) (?=.*[^\w\*])(?=.*[A-Z]).{4,}'
+								title='password must contain at least 4 characters including at least one uppercase, one lowercase letter and one special character.'
+								className='form-control'
+								placeholder='Enter your password'
+							/>
+							<button type='button' onClick={toggleButton} className='eye'>
+								<FontAwesomeIcon icon={mode} />
+							</button>
+						</div>
+					</div>
 
-  <div className='card'>
-    <h1>Log in</h1>
-    <form id='form1' action="./Home">
-      <div className='input1'>
-        <lable forHtml="email" className="email ">Email address</lable>
-        
-        <input type="email" required className="form-control " id="exampleFormControlInput1" placeholder="name@gmail.com" />
-      </div>
-      <div className="input2">
-        <lable forHtml="password" className="password">Password</lable>
-        <div className='buttonIn'>
-        <input id="eye" type={visibility} required pattern="(?=.*[a-z]) (?=.*[^\w\*])(?=.*[A-Z]).{4,}" title="password must contain at least 4 characters including at least one uppercase, one lowercase letter and one special character." className="form-control"  placeholder="Enter your password"/>
-        <button type='button' onClick={toggleButton} className='eye'><FontAwesomeIcon  icon={mode} /></button>
-       </div>
-       </div>
-        
-          
-      
-      <a href='/' id='Forget'>Forget password?</a>
-      <button type='submit' form='form1' className="loginform-button">LOGIN</button>
-      </form>
-      
-      <hr />
-      <form action="./Home" >
-        <button type='submit' className='icons'><FontAwesomeIcon style={myStyle} icon={faGoogle} /></button>
-        <button type='submit' className='icons'><FontAwesomeIcon style={myStyle} icon={faFacebook} /></button>
-      <button type='submit' className='icons'><FontAwesomeIcon style={myStyle} icon={faLinkedin} /></button>
-      </form>
-      <p style={{fontSize:"14px"}}>Need an Account? <Link to="/Signup">Sign up</Link></p>
-      
-      
-  </div>
-</div>  
-  )
-}
+					<a href='/' id='Forget'>
+						Forget password?
+					</a>
+					<button type='submit' form='form1' className='loginform-button'>
+						LOGIN
+					</button>
+				</form>
 
-export default Login
+				<hr />
+				<form action='./Home'>
+					<button type='submit' className='icons'>
+						<FontAwesomeIcon style={myStyle} icon={faGoogle} />
+					</button>
+					<button type='submit' className='icons'>
+						<FontAwesomeIcon style={myStyle} icon={faFacebook} />
+					</button>
+					<button type='submit' className='icons'>
+						<FontAwesomeIcon style={myStyle} icon={faLinkedin} />
+					</button>
+				</form>
+				<p style={{ fontSize: '14px' }}>
+					Need an Account? <Link to='/Signup'>Sign up</Link>
+				</p>
+			</div>
+		</div>
+	);
+};
+
+export default Login;
--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,51 +1,92 @@
-import React from 'react'
-import {Link} from "react-router-dom";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import {  faGoogle,faFacebook,faLinkedin } from '@fortawesome/free-brands-svg-icons' // <-- import styles to be used
-
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+	faGoogle,
+	faFacebook,
+	faLinkedin,
+} from '@fortawesome/free-brands-svg-icons'; // <-- import styles to be used
 
 export default function Signup() {
-  const myStyle={
-    margin:"10px 10px 0 10px",
-    height:"10%",
-    width:"10%"
-  }
-  return (
-    <div>
-      <div className='card'>
-    <h1>Sign up</h1>
-    <form action="./Home.js">
-    <div className='input0'>
-        <lable forHtml="email" className="UserName">User name</lable>
-        
-        <input type="email" required className="form-control" id="exampleFormControlInput1" placeholder="name@gmail.com" />
-      </div>
-      <div className='input1'>
-        <lable forHtml="email" className="email">Email address</lable>
-        
-        <input type="email" required className="form-control" id="exampleFormControlInput1" placeholder="name@gmail.com" />
-      </div>
-      <div className='input2'>
-        <lable forHtml="password" className="password">Password</lable>
-        
-        <input type="password" required pattern="(?=.*[a-z])(?=.*[^\w\*])(?=.*[A-Z]).{4,}" title="password must contain at least 4 characters including at least one uppercase, one lowercase letter and one special character." className="form-control" id="Enter your password" placeholder="Enter your password" />
-      </div>
-      <div className='input3'>
-        <lable forHtml="password" className="password">Confirm Password</lable>
-        
-        <input type="password" required pattern="(?=.*[a-z])(?=.*[^\w\*])(?=.*[A-Z]).{4,}" title="password must contain at least 4 characters including at least one uppercase, one lowercase letter and one special character." className="form-control" id="Enter your password" placeholder="Enter your password" />
-      </div>
-      
-      <button type='submit' className="loginform-button">Sign up</button>
-      </form>
-      <hr />
-      <FontAwesomeIcon style={myStyle} icon={faGoogle} />
-      <FontAwesomeIcon style={myStyle} icon={faFacebook} />
-      <FontAwesomeIcon style={myStyle} icon={faLinkedin} />
-      <p>Already a user?<Link to="/Login"> Log in</Link></p>
-      
-      
-  </div>
-    </div>
-  )
+	const myStyle = {
+		margin: '10px 10px 0 10px',
+		height: '10%',
+		width: '10%',
+	};
+	return (
+		<div>
+			<div className='card'>
+				<h1>Sign up</h1>
+				<form action='./Home.js'>
+					<div className='input0'>
+						<lable forHtml='email' className='UserName'>
+							User name
+						</lable>
+
+						<input
+							type='email'
+							required
+							className='form-control'
+							id='exampleFormControlInput1'
+							placeholder='name@gmail.com'
+						/>
+					</div>
+					<div className='input1'>
+						<lable forHtml='email' className='email'>
+							Email address
+						</lable>
+
+						<input
+							type='email'
+							required
+							className='form-control'
+							id='exampleFormControlInput1'
+							placeholder='name@gmail.com'
+						/>
+					</div>
+					<div className='input2'>
+						<lable forHtml='password' className='password'>
+							Password
+						</lable>
+
+						<input
+							type='password'
+							required
+							pattern='(?=.*[a-z])(?=.*[^\w\*])(?=.*[A-Z]).{4,}'
+							title='password must contain at least 4 characters including at least one uppercase, one lowercase letter and one special character.'
+							className='form-control'
+							id='Enter your password'
+							placeholder='Enter your password'
+						/>
+					</div>
+					<div className='input3'>
+						<lable forHtml='password' className='password'>
+							Confirm Password
+						</lable>
+
+						<input
+							type='password'
+							required
+							pattern='(?=.*[a-z])(?=.*[^\w\*])(?=.*[A-Z]).{4,}'
+							title='password must contain at least 4 characters including at least one uppercase, one lowercase letter and one special character.'
+							className='form-control'
+							id='Enter your password'
+							placeholder='Enter your password'
+						/>
+					</div>
+
+					<button type='submit' className='loginform-button'>
+						Sign up
+					</button>
+				</form>
+				<hr />
+				<FontAwesomeIcon style={myStyle} icon={faGoogle} />
+				<FontAwesomeIcon style={myStyle} icon={faFacebook} />
+				<FontAwesomeIcon style={myStyle} icon={faLinkedin} />
+				<p>
+					Already a user?<Link to='/'> Log in</Link>
+				</p>
+			</div>
+		</div>
+	);
 }
