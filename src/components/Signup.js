4e434425bf33f727a43a62f2110a64454e16cbff
Praneeth <pannatipraneethvnit@gmail.com>
2022-07-16 15:23:59 +0530

    signup page

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import './Signup.css';
+import '../index.css';
 import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {
@@ -18,23 +20,14 @@ export default function Signup() {
 			<div className='card'>
 				<h1>Sign up</h1>
 				<form action='./Home.js'>
+					
 					<div className='input0'>
-						<lable forHtml='email' className='UserName'>
-							User name
-						</lable>
-
-						<input
-							type='email'
-							required
-							className='form-control'
-							id='exampleFormControlInput1'
-							placeholder='name@gmail.com'
-						/>
+						<lable forHtml='email' className='email'>User name</lable>
+						<input type='text' required className='form-control' id='exampleFormControlInput1' placeholder='Enter your username' />
 					</div>
+					
 					<div className='input1'>
-						<lable forHtml='email' className='email'>
-							Email address
-						</lable>
+						<lable forHtml='email' className='email'>Email address</lable>
 
 						<input
 							type='email'
@@ -44,10 +37,9 @@ export default function Signup() {
 							placeholder='name@gmail.com'
 						/>
 					</div>
+					
 					<div className='input2'>
-						<lable forHtml='password' className='password'>
-							Password
-						</lable>
+						<lable forHtml='password' className='password'>Password</lable>
 
 						<input
 							type='password'
@@ -59,10 +51,9 @@ export default function Signup() {
 							placeholder='Enter your password'
 						/>
 					</div>
+					
 					<div className='input3'>
-						<lable forHtml='password' className='password'>
-							Confirm Password
-						</lable>
+						<lable forHtml='Confirm Password' className='confirmpassword'>Confirm Password</lable>
 
 						<input
 							type='password'
@@ -71,20 +62,18 @@ export default function Signup() {
 							title='password must contain at least 4 characters including at least one uppercase, one lowercase letter and one special character.'
 							className='form-control'
 							id='Enter your password'
-							placeholder='Enter your password'
+							placeholder='Enter your password again'
 						/>
 					</div>
 
-					<button type='submit' className='loginform-button'>
-						Sign up
-					</button>
+					<button type='submit' className='loginform-button'>Sign up</button>
 				</form>
 				<hr />
 				<FontAwesomeIcon style={myStyle} icon={faGoogle} />
 				<FontAwesomeIcon style={myStyle} icon={faFacebook} />
 				<FontAwesomeIcon style={myStyle} icon={faLinkedin} />
 				<p>
-					Already a user?<Link to='/'> Log in</Link>
+					Already a user?<Link to='/Login'> Log in</Link>
 				</p>
 			</div>
 		</div>
